4038924aa52ceb7bac2418e0bda8bdf0801869f1
davorcelcic <davor.celcic@gmail.com>
2025-08-12 21:44:03 +0200

    ading product in basket for current user

--- AbySalto.Mid/Controllers/BasketController.cs
+++ AbySalto.Mid/Controllers/BasketController.cs
@@ -1,8 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AbySalto.Mid.WebApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace AbySalto.Mid.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class BasketController : Controller
     {
+        private readonly BasketService _basketService;
+
+        public BasketController(BasketService basketService)
+        {
+            _basketService = basketService;
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> AddProduct([FromQuery] int productId, [FromQuery] int quantity = 1)
+        {
+            var username = User.Identity?.Name;
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(userId))
+                return Unauthorized(new { Message = "User is not authenticated." });
+            if (quantity <= 0)
+                return BadRequest("Quantity must be greater than zero.");
+            if (productId <= 0)
+                return BadRequest("Invalid product ID.");
+            int numberId;
+            bool success = int.TryParse(userId, out numberId);
+            if (!success)
+                return BadRequest("User ID is not valid.");
+            await _basketService.AddProductAsync(numberId, productId, quantity);
+            return Ok();
+        }
     }
 }
---
+++ AbySalto.Mid/Services/BasketService.cs
@@ -0,0 +1,31 @@
+﻿using AbySalto.Mid.WebApi.Data;
+using AbySalto.Mid.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbySalto.Mid.WebApi.Services
+{
+    public class BasketService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BasketService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddProductAsync(int userId, int productId, int quantity)
+        {
+            var basketItem = await _context.BasketItems.FirstOrDefaultAsync(b => b.UserId == userId && b.ProductId == productId);
+            if (basketItem != null)
+            {
+                basketItem.Quantity += quantity;
+            }
+            else
+            {
+                basketItem = new BasketItem { UserId = userId, ProductId = productId, Quantity = quantity };
+                _context.BasketItems.Add(basketItem);
+            }
+            await _context.SaveChangesAsync();
+        }
+    }
+}
