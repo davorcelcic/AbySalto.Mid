dea4a779971da74503003760ba9bf6f8020076f5
davorcelcic <davor.celcic@gmail.com>
2025-08-12 21:14:26 +0200

    product controller route - getAll

--- AbySalto.Mid/Controllers/ProductController.cs
+++ AbySalto.Mid/Controllers/ProductController.cs
@@ -1,8 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AbySalto.Mid.WebApi.Models;
+using AbySalto.Mid.WebApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace AbySalto.Mid.Controllers
 {
+    [ApiController]
+    [Route("api/product")]
     public class ProductController : Controller
-    {
+    {       
+
+        [HttpGet("get-all-products")]
+        public async Task<IActionResult> GetProducts()
+        {
+            ProductService productService = new ProductService(new MemoryCache(new MemoryCacheOptions()), new HttpClient());
+            ProductResponse productResponse = await productService.GetProductsAsync();
+            return Ok(productResponse.Products);
+        }
+
+       
     }
 }
