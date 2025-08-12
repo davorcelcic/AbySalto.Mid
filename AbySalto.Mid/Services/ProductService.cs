58a6a3c2599795bbe80c9f1c5f07a55cd8a74da8
davorcelcic <davor.celcic@gmail.com>
2025-08-12 21:26:46 +0200

    product paging, filtering, sort, get by ID (service and controller)

--- AbySalto.Mid/Services/ProductService.cs
+++ AbySalto.Mid/Services/ProductService.cs
@@ -21,6 +21,29 @@ namespace AbySalto.Mid.WebApi.Services
             return await GetProductsStringAsync($"https://dummyjson.com/products", cacheKey);
         }
 
+        public async Task<Product> GetProductAsync(int productId)
+        {
+            string cacheKey = $"products_{productId}";
+            string link = $"https://dummyjson.com/products/{productId}";
+
+            Product? product;
+            if (!_cache.TryGetValue(cacheKey, out product))
+            {
+                var response = await _httpClient.GetAsync(link);
+                response.EnsureSuccessStatusCode();
+                var jsonString = await response.Content.ReadAsStringAsync();
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+                var jsonDocument = JsonDocument.Parse(jsonString);
+                product = JsonSerializer.Deserialize<Product>(jsonString, options) ?? new Product();
+
+                _cache.Set(cacheKey, product, TimeSpan.FromMinutes(10));
+            }
+            return product;
+        }
+
         private async Task<ProductResponse> GetProductsStringAsync(string link, string cacheKey)
         {
             ProductResponse? productResponse;
