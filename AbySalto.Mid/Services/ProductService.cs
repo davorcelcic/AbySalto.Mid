acf78ee360f8f6d32cfda82ee42263192a24b106
davorcelcic <davor.celcic@gmail.com>
2025-08-12 21:11:41 +0200

    getProducts, productResponse for getting json format from dummy site

---
+++ AbySalto.Mid/Services/ProductService.cs
@@ -0,0 +1,43 @@
+﻿using AbySalto.Mid.WebApi.Models;
+using Microsoft.Extensions.Caching.Memory;
+using System.Text.Json;
+
+namespace AbySalto.Mid.WebApi.Services
+{
+    public class ProductService
+    {
+        private readonly IMemoryCache _cache;
+        private readonly HttpClient _httpClient;
+
+        public ProductService(IMemoryCache cache, HttpClient httpClient)
+        {
+            _cache = cache;
+            _httpClient = httpClient;
+        }
+
+        public async Task<ProductResponse> GetProductsAsync()
+        {
+            string cacheKey = $"products_all";
+            return await GetProductsStringAsync($"https://dummyjson.com/products", cacheKey);
+        }
+
+        private async Task<ProductResponse> GetProductsStringAsync(string link, string cacheKey)
+        {
+            ProductResponse? productResponse;
+            if (!_cache.TryGetValue(cacheKey, out productResponse))
+            {
+                var response = await _httpClient.GetAsync(link);
+                response.EnsureSuccessStatusCode();
+                var jsonString = await response.Content.ReadAsStringAsync();
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+                var jsonDocument = JsonDocument.Parse(jsonString);
+                productResponse = JsonSerializer.Deserialize<ProductResponse>(jsonString, options) ?? new ProductResponse();
+                _cache.Set(cacheKey, productResponse, TimeSpan.FromMinutes(10));
+            }
+            return productResponse;
+        }
+    }
+}
